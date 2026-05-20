f8f715b3149302b2e8c1bf7e3750ef826b700dcf
PS Invest <ariel@ps-invest.co.il>
2026-05-20 09:45:02 +0000

    fix: bind to 0.0.0.0 for Railway deployment

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/health', (req, res) => res.send('OK'));
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Mortgage Portal running on port ${PORT}`);
 });
